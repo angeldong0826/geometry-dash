62d716446a4d634b3d3af19610deb69eeded4296
angeldong0826 <adong6@illinois.edu>
2021-04-20 22:21:21 -0500

    Fixed game over.

--- src/player_manager.cpp
+++ src/player_manager.cpp
@@ -50,11 +50,18 @@ namespace geometrydash {
   void PlayerManager::IsGameOver(Player &player, std::vector<Obstacle> &obstacles) {
 
     for (Obstacle &obstacle : obstacles) {
-      if (((player.GetPosition().x + static_cast<float>(kPlayerWidth) / 2) >= (obstacle.GetPosition().x - 
-                                     static_cast<float>(obstacle.GetWidth()) / 2)) && ((player.GetPosition().y + 
-                                     static_cast<float>(kPlayerWidth) / 2) <= obstacle.GetPosition().y 
-                                     && player.GetPosition().y >= obstacle.GetPosition().y - 
-                                     static_cast<float>(obstacle.GetHeight()))) {
+//      if ((obstacle.GetPosition().x - (obstacle.GetWidth()/2 + kPlayerWidth/2)) <= player.GetPosition().x
+//          && player.GetPosition().x <= (obstacle.GetPosition().x + (kPlayerWidth/2 + obstacle.GetWidth()/2))
+//          && (kLinePosition + kPlayerWidth/2 - (obstacle.GetHeight())) <= player.GetPosition().y 
+//          && player.GetPosition().y <= kLinePosition + kPlayerWidth/2) {
+//        game_over_ = true;
+//      }
+  
+      if (((player.GetPosition().x + static_cast<float>(kPlayerWidth) / 2) >= (obstacle.GetPosition().x - static_cast<float>(obstacle.GetWidth()) / 2))
+                                     && ((player.GetPosition().y + static_cast<float>(kPlayerWidth) / 2) <= obstacle.GetPosition().y) 
+                                     && (player.GetPosition().y > obstacle.GetPosition().y - static_cast<float>(obstacle.GetHeight()))
+                                     && (player.GetPosition().x <= (obstacle.GetPosition().x + static_cast<float>((static_cast<float>(kPlayerWidth)/2 + 
+                                     static_cast<float>(obstacle.GetWidth())/2))))) {
         game_over_ = true;
       }
     }
